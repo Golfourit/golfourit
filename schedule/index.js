51b45fa844ee6fc30cf34e2328cb04caa6dc06b2
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2020-11-29 19:55:55 +0800

    update

---
+++ schedule/index.js
@@ -0,0 +1,26 @@
+const schedule = require('node-schedule');
+const express = require('express')
+const request = require('request')
+const app = express()
+
+let a = new Map()
+
+app.route('/schedule',(req,res)=>{
+
+    const {cron_expression,url,type} = req.params
+
+    if(a.has(type))a.get(type).cancel()
+
+    const j = schedule.scheduleJob(new Date(...cron_expression), function(){
+        request(url)
+    });
+    
+    a.set(type,j)
+    res.send('ok')
+    
+})
+app.listen(5001,()=>{
+    console.log("listen on 5001")
+})
+
+
