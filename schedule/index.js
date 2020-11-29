b3e0da4e22ea453f4368d2eb425b1b17b29fe87f
吳俊毅 <40282112+HankTwTw@users.noreply.github.com>
2020-11-29 20:02:21 +0800

    Update index.js

--- schedule/index.js
+++ schedule/index.js
@@ -6,12 +6,13 @@ const app = express()
 let a = new Map()
 
 app.route('/schedule',(req,res)=>{
-
+    console.log("hi")
     const {cron_expression,url,type} = req.params
 
     if(a.has(type))a.get(type).cancel()
 
     const j = schedule.scheduleJob(new Date(...cron_expression), function(){
+        console.log("send")
         request(url)
     });
     
